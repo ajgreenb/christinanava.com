cbe72bcd279b92f4ce69f4d2c83ab823ac3e3733
Aaron Greenberg <aaron@greenberg.io>
2017-03-19 22:38:41 -0400

    Parallelize upload.py and resize.py

--- resize.py
+++ resize.py
@@ -1,3 +1,4 @@
+import multiprocessing
 import os
 import re
 import shutil
@@ -43,63 +44,82 @@ def ignore(path, names):
     """
 
     if path == MEDIA_DIR: return []
-    return filter(lambda n: os.path.isfile(os.path.join(path, n)), names)
+    return filter(
+        lambda n: os.path.isfile(os.path.join(path, n)) and is_image(n),
+        names
+    )
 
 # Clean up and regenerate processed media-file directory.
 if os.path.isdir(PROCESSED_DIR):
     shutil.rmtree(PROCESSED_DIR)
 shutil.copytree(MEDIA_DIR, PROCESSED_DIR, ignore = ignore)
 
-for dirpath, dirnames, filenames in os.walk(MEDIA_DIR):
+def resize(png_path):
+    """
+    A function that resizes an image into full-, mobile-, and thumbnail-sized
+    versions. It's used as a target function for a multiprocessing Pool.
+    """
 
-    # Ignore PNGs in the top-level directory; these don't need to be full-size
-    # and thus resized.
-    if dirpath == MEDIA_DIR: continue
+    base = os.path.basename(png_path)
+    dirpth = os.path.dirname(png_path)
+    name, _ = os.path.splitext(base)
+    name = slugify(name)
 
-    # Filter any possible non-image files.
-    pngs = filter(is_image, filenames)
+    print 'Resizing {}...'.format(png_path)
+    with Image.open(png_path) as img:
 
-    # Iterate over each full-sized PNG to resize it.
-    for png_name in pngs:
+        orig_width, orig_height = img.size
 
-        name, _ = os.path.splitext(png_name)
-        name = slugify(name)
+        full_path   = '.' + os.path.join(dirpth, name + '-{0}x{1}.png')
+        mobile_path = '.' + os.path.join(dirpth, name + '-{0}x{1}.mobile.png')
+        thumb_path  = '.' + os.path.join(dirpth, name + '.thumb.png')
 
-        # Path to full-sized image.
-        png_path = os.path.join(dirpath, png_name)
+        # Resize over-sized images.
+        if orig_width > full_width:
+            full_height = int(round(orig_height * full_width / orig_width))
+            full = img.resize((full_width, full_height), LANCZOS)
+            full.save(full_path.format(full_width, full_height))
+            full.close()
+        else:
+            img.save(full_path.format(orig_width, orig_height))
 
-        print 'Resizing {}...'.format(png_path)
-        with Image.open(png_path) as img:
+        # Generate smaller images for mobile phones.
+        if orig_width > mobile_width:
+            mobile_height = int(round(orig_height * mobile_width / orig_width))
+            mobile = img.resize((mobile_width, mobile_height), LANCZOS)
+            mobile.save(mobile_path.format(mobile_width, mobile_height))
+            mobile.close()
+        else:
+            img.save(mobile_path.format(orig_width, orig_height))
 
-            orig_width, orig_height = img.size
+        # Generate thumbnail-sized copies of each image.
+        if orig_width > thumb_width:
+            thumb_height = int(round(orig_height * thumb_width / orig_width))
+            # Thumbnail method resizes the image in place.
+            img.thumbnail((thumb_width, thumb_height), LANCZOS)
+            img.save(thumb_path)
+        else:
+            img.save(thumb_path)
 
-            full_path   = '.' + os.path.join(dirpath, name + '-{0}x{1}.png')
-            mobile_path = '.' + os.path.join(dirpath, name + '-{0}x{1}.mobile.png')
-            thumb_path  = '.' + os.path.join(dirpath, name + '.thumb.png')
+if __name__ == '__main__':
 
-            # Resize over-sized images.
-            if orig_width > full_width:
-                full_height = int(round(orig_height * full_width / orig_width))
-                full = img.resize((full_width, full_height), LANCZOS)
-                full.save(full_path.format(full_width, full_height))
-                full.close()
-            else:
-                img.save(full_path.format(orig_width, orig_height))
+    # Store a list of all images to resize.
+    to_resize = []
 
-            # Generate smaller images for mobile phones.
-            if orig_width > mobile_width:
-                mobile_height = int(round(orig_height * mobile_width / orig_width))
-                mobile = img.resize((mobile_width, mobile_height), LANCZOS)
-                mobile.save(mobile_path.format(mobile_width, mobile_height))
-                mobile.close()
-            else:
-                img.save(mobile_path.format(orig_width, orig_height))
+    for dirpath, dirnames, filenames in os.walk(MEDIA_DIR):
 
-            # Generate thumbnail-sized copies of each image.
-            if orig_width > thumb_width:
-                thumb_height = int(round(orig_height * thumb_width / orig_width))
-                # Thumbnail method resizes the image in place.
-                img.thumbnail((thumb_width, thumb_height), LANCZOS)
-                img.save(thumb_path)
-            else:
-                img.save(thumb_path)
+        # Ignore PNGs in the top-level directory; these don't need to be full-size
+        # and thus resized.
+        if dirpath == MEDIA_DIR: continue
+
+        # Filter any possible non-image files.
+        pngs = filter(is_image, filenames)
+
+        # Join the image name with its directory path.
+        pngs = map(lambda p: os.path.join(dirpath, p), pngs)
+
+        to_resize.extend(pngs)
+
+    # Resize all the images in parallel.
+    p = multiprocessing.Pool(multiprocessing.cpu_count())
+    p.map(resize, to_resize)
--- upload.py
+++ upload.py
@@ -1,5 +1,6 @@
 import mimetypes
 import os
+from multiprocessing.pool import ThreadPool
 
 import boto3
 
@@ -9,18 +10,38 @@ s3 = boto3.client('s3')
 def is_woff(path):
     return path.endswith('woff') or path.endswith('woff2')
 
-for dirpath, dirnames, filenames in os.walk(SITE_DIR):
-    for filename in filenames:
-        path = os.path.join(dirpath, filename)
-        key = path.replace(SITE_DIR + '/', '')
-        mime, _ = mimetypes.guess_type(path)
-        if mime is None:
-            if is_woff(path):
-                mime = 'application/x-font-woff'
-            else:
-                raise Exception('Could not guess Content-Type for: ' + path)
-        print 'Uploading {}...'.format(key)
-        s3.upload_file(path, 'christinanava.com', key, ExtraArgs = {
+def upload_file(args):
+    filepath, mime = args
+    key = filepath.replace(SITE_DIR + '/', '')
+    print 'Uploading {}...'.format(key)
+    s3.upload_file(
+        filepath,
+        'aws-website-christinanavacom-zkmp7',
+        key,
+        ExtraArgs = {
             'ContentType': mime,
             'ACL': 'public-read',
-        })
+        }
+    )
+
+
+if __name__ == '__main__':
+
+    # Build up a list of files that will be uploaded to Amazon S3.
+    to_upload = []
+
+    for dirpath, dirnames, filenames in os.walk(SITE_DIR):
+        for filename in filenames:
+            if filename == '.DS_Store': continue
+            path = os.path.join(dirpath, filename)
+            mime, _ = mimetypes.guess_type(path)
+            if mime is None:
+                if is_woff(path):
+                    mime = 'application/x-font-woff'
+                else:
+                    raise Exception('Could not guess Content-Type for: ' + path)
+            to_upload.append((path, mime))
+
+    # Upload site files to S3 in parallel.
+    pool = ThreadPool(processes = 10)
+    pool.map(upload_file, all_files)
